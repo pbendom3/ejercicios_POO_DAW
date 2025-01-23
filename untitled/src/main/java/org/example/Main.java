2641bb376e55be5c04bac885072cf9ee2756245d
A600\Patricia <p.benaventedomenec@edu.gva.es>
2025-01-23 21:08:09 +0100

    ++

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,13 +10,73 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        Paciente paciente1 = new Paciente("Luis",45,'H',90,1.90);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("nombre?");
+        String nombre = teclado.next();
+        System.out.println("edad?");
+        int edad = teclado.nextInt();
+        System.out.println("género?");
+        char genero = teclado.next().charAt(0);
+        System.out.println("peso?");
+        double peso = teclado.nextDouble();
+        System.out.println("altura?");
+        double altura = teclado.nextDouble();
+
+        Paciente paciente1 = new Paciente();
+        Paciente paciente2 = new Paciente(nombre,edad,genero);
+        Paciente paciente3 = new Paciente(nombre,edad,genero,peso,altura);
+
+        paciente1.setNombre("Llados");
+        paciente1.setEdad(40);
+        paciente1.setGenero('H');
+        paciente1.setPeso(120);
+        paciente1.setAltura(1.75);
+
         paciente1.imprimirInfo();
-        Paciente paciente2 = new Paciente();
         paciente2.imprimirInfo();
+        paciente3.imprimirInfo();
 
+        mostrarMensajeIMC(paciente1);
+        mostrarMensajeIMC(paciente2);
+        mostrarMensajeIMC(paciente3);
 
+        mayorEdad(paciente1);
+        mayorEdad(paciente2);
+        mayorEdad(paciente3);
+
+        System.out.println(paciente1);
 
 
     }
+
+    public static void mostrarMensajeIMC(Paciente paciente){
+
+        int peso = paciente.calcularIMC();
+
+        switch(peso){
+            case Paciente.BAJO_PESO:
+                System.out.println("El paciente " + paciente.getNombre() +  " está por debajo del peso ideal");
+                break;
+            case Paciente.PESO_IDEAL:
+                System.out.println("El paciente " + paciente.getNombre() + " está en su peso ideal");
+                break;
+            case Paciente.SOBREPESO:
+                System.out.println("El paciente " + paciente.getNombre() + " está por encima de su peso ideal");
+                break;
+        }
+
+    }
+
+    public static void mayorEdad(Paciente paciente){
+
+        if(paciente.esMayorDeEdad()){
+            System.out.println("El paciente " + paciente.getNombre() + " es mayor de edad");
+        }else{
+            System.out.println("El paciente " + paciente.getNombre() + " no es mayor de edad --> " + paciente.getEdad());
+        }
+
+    }
+
+
 }
--- untitled/src/main/java/org/example/Paciente.java
+++ untitled/src/main/java/org/example/Paciente.java
@@ -5,6 +5,11 @@ import java.util.Random;
 public class Paciente {
 
     public final static char GENERO_DEF = 'X';
+    public final static int BAJO_PESO = -1;
+    public final static int PESO_IDEAL = 0;
+    public final static int SOBREPESO = 1;
+    public static final int EDAD_ADULTA = 18;
+
     private String nombre;
     private int edad;
     private String dni;
@@ -18,10 +23,14 @@ public class Paciente {
         this("",0,GENERO_DEF,0.0,0.0);
     }
 
+    public Paciente(String nombre, int edad, char genero){
+        this(nombre,edad,genero,0.0,0.0);
+    }
+
     public Paciente(String nombre, int edad, char genero, double peso, double altura){
         this.nombre=nombre;
         this.edad=edad;
-        this.genero=genero;
+        setGenero(genero);
         this.peso=peso;
         this.altura=altura;
         generarDni();
@@ -45,7 +54,87 @@ public class Paciente {
 
     }
 
+    public int calcularIMC(){
 
+        double resultado = peso / Math.pow(altura,2);
+
+        if(resultado<20){
+            return BAJO_PESO;
+        } else if (resultado>25) {
+            return SOBREPESO;
+        }else{
+            return PESO_IDEAL;
+        }
+
+    }
+
+    public boolean esMayorDeEdad(){
+
+        if(edad>=EDAD_ADULTA){
+            return true;
+        }
+
+        return false;
+    }
+
+    private void comprobarGenero(char genero){
+
+        if(genero=='H' || genero=='M'){
+            this.genero=genero;
+        }else{
+            this.genero=GENERO_DEF;
+        }
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+
+    public String getDni(){
+        return dni;
+    }
+
+    public char getGenero(){
+        return genero;
+    }
+
+    public void setGenero(char genero){
+        comprobarGenero(genero);
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+
+    public void setPeso(double peso){
+        this.peso=peso;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+
+    public void setAltura(double altura){
+        this.altura=altura;
+    }
+
+    @Override
+    public String toString(){
+        return "Paciente : [ nombre=" + this.nombre + " edad=" + this.edad + " genero=" + this.genero + " peso=" + this.peso + " altura=" + this.altura + "]";
+    }
 
 
 }
